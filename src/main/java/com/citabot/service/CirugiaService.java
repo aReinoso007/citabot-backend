e2e33a1d2cf141a9fa74e3ff867fe0d878529fdf
Alex Reinoso <ralexjessiel@gmail.com>
2021-11-18 21:12:12 -0500

    Crud cirugia

---
+++ src/main/java/com/citabot/service/CirugiaService.java
@@ -0,0 +1,58 @@
+package com.citabot.service;
+
+import com.citabot.interfaceService.ICirugiaService;
+import com.citabot.interfaces.ICirugia;
+import com.citabot.model.Cirugia;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class CirugiaService implements ICirugiaService {
+
+    @Autowired
+    private ICirugia data;
+
+    @Override
+    public List<Cirugia> listar() {
+        return (List<Cirugia>) data.findAll();
+    }
+
+    @Override
+    public Cirugia save(Cirugia cirugia) {
+        Cirugia c = new Cirugia();
+        try{
+            c = data.save(cirugia);
+        }catch (Error e){
+            System.out.printf("Error insertando: ", e.getCause());
+        }
+
+
+        return c;
+    }
+
+    @Override
+    public Cirugia update(Cirugia cirugia) {
+        Cirugia c = new Cirugia();
+
+        try{
+            c = data.save(cirugia);
+
+        }catch (Error e){
+            System.out.printf("Error updating: ", e.getMessage());
+        }
+        return c;
+    }
+
+    @Override
+    public String delete(int id) {
+        Cirugia c = new Cirugia();
+        String message = "Sucess";
+        try{
+            data.deleteById(id);
+        }catch (Error e){
+            System.out.printf("Error deleting: ", e.getMessage());
+            message = "Failed to delete: " + data.findById(id);
+        }
+        return message;
+    }
+}
