bcb507b32c38582fc04d6cb230eb01b8ec2ba30f
Alex Reinoso <ralexjessiel@gmail.com>
2021-11-18 21:54:13 -0500

    servicio clinica

--- src/main/java/com/citabot/interfaces/IClinica.java
+++ src/main/java/com/citabot/interfaces/IClinica.java
@@ -3,5 +3,9 @@ package com.citabot.interfaces;
 import com.citabot.model.Clinica;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface IClinica extends CrudRepository<Clinica, Integer> {
+
+    List<Clinica> findClinicaByNombreClinica(String nombre);
 }
