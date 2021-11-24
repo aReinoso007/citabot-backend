faba2ac6e58264e3e982f169cf5ee4867c5fe28a
Christian Zhirzhan <34308679+damian1998@users.noreply.github.com>
2021-11-24 00:41:58 -0500

    Actualizar especialidad y subespecialidad

--- src/main/java/com/citabot/controller/SubespecialdiadController.java
+++ src/main/java/com/citabot/controller/SubespecialdiadController.java
@@ -1,4 +1,62 @@
 package com.citabot.controller;
 
+
+
+import com.citabot.interfaceService.ISubespecialidadService;
+
+import com.citabot.model.Subespecialidad;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "http://localhost:8090")
+@RestController
+@RequestMapping("/subespecialidad")
+
+
 public class SubespecialdiadController {
+
+    @Autowired
+    ISubespecialidadService service;
+
+
+
+    @GetMapping(produces = "application/json")
+    public List<Subespecialidad> listar(){
+        return service.listar();
+    }
+
+    @GetMapping("/query")
+    public List<Subespecialidad> getByNombre(@RequestParam String nombre){
+        return service.listarByNombre(nombre);
+    }
+
+    @GetMapping(path = "/{id}")
+    Optional<Subespecialidad> getById(@PathVariable("id") int id){
+        return service.findById(id);
+    }
+
+    @PostMapping(consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
+    public Subespecialidad guardarSubespecialidad(@RequestParam("nombre") String nombre, @RequestParam("especialidad_id") int especialidad_id){
+        Subespecialidad subespecialidad = new Subespecialidad();
+
+        try{
+            subespecialidad=service.save(nombre,especialidad_id);
+            return subespecialidad;
+        }catch (Error error){
+            System.out.printf("Error posting subespecialdad: ", error.getMessage());
+        }
+        return subespecialidad;
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public String deleteById(@PathVariable("id") int id){
+        String resp = service.delete(id);
+        return resp;
+    }
+
+
 }
--- src/main/java/com/citabot/interfaceService/IEspecialidadService.java
+++ src/main/java/com/citabot/interfaceService/IEspecialidadService.java
@@ -1,13 +1,16 @@
 package com.citabot.interfaceService;
 
+import com.citabot.model.Enfermedad;
 import com.citabot.model.Especialidad;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IEspecialidadService {
 
     public List<Especialidad> listar();
     public List<Especialidad> listarByNombre(String nombre);
+    public Optional<Especialidad> findById(int id);
     public Especialidad save(Especialidad especialidad);
     public Especialidad edit(Especialidad especialidad);
     public String delete(int id);
