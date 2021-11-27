7c2e2af4cb4b50692b8c1e34911b713714277730
Christian Zhirzhan <34308679+damian1998@users.noreply.github.com>
2021-11-27 17:48:24 -0500

    update medico

--- src/main/java/com/citabot/service/MedicoService.java
+++ src/main/java/com/citabot/service/MedicoService.java
@@ -57,16 +57,7 @@ public class MedicoService implements IMedicoService {
         return message;
     }
 
-    @Override
-    public List<Medico> buscarCedula(int idEspecialidad) {
-        System.out.println("Consulta Realizada...");
-        Query nativeQuery = em.createNativeQuery(
-                "SELECT id, numero, operadora, tipo, usuario_cedula FROM usuario, telefono WHERE telefono.usuario_cedula=usuario.cedula and usuario.cedula= ?",
-                Telefono.class);
-        nativeQuery.setParameter(1, cedula);
-        System.out.println("Consulta Realizada...");
-        return (List<Telefono>) nativeQuery.getResultList();
-    }
+
 
 
 
