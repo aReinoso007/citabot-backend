f34c4b6e92e1302a562145e6e173e2692ddcedf2
Alex Reinoso <ralexjessiel@gmail.com>
2022-01-03 16:51:07 -0500

    el controler de horario

--- src/main/java/com/citabot/controller/HorarioController.java
+++ src/main/java/com/citabot/controller/HorarioController.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api/public/horario")
+@RequestMapping("/api/private/horario")
 @Slf4j
 public class HorarioController {
 
