addae75cda2c3300fd4068c65fd034ba0d6aaa1c
Alex Reinoso <ralexjessiel@gmail.com>
2022-01-11 21:48:16 -0500

    cambios para subir a aws

--- src/main/java/com/citabot/controller/MainController.java
+++ src/main/java/com/citabot/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class MainController {
-    @RequestMapping("/")
+    @RequestMapping("/welcome")
     public String getInit(){
         return "Hola";
     }
